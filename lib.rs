dd4c6a865fa017deaa9ccdc5612210692bec0842
SelimEris <98539759+SelimEris@users.noreply.github.com>
2023-12-30 16:20:06 -0500

    Delete lib.rs

--- lib.rs
+++
@@ -1,7 +0,0 @@
-extern crate nom;
-
-pub mod interpreter;
-pub mod parser;
-
-pub use self::parser::{program, Node, conditional_expression}; // Added conditional_expression here
-pub use self::interpreter::{start_interpreter, Value};
